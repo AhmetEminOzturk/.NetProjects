f7bd57764e21088289d5dc68ea85db4b172cedf0
AhmetEminOzturk <116569921+AhmetEminOzturk@users.noreply.github.com>
2023-03-31 23:40:17 +0300

    update

--- Project3-SpryStore-Core-CodeFirst/SpryStore.PresentationLayer/SpryStore.PresentationLayer/Startup.cs
+++ Project3-SpryStore-Core-CodeFirst/SpryStore.PresentationLayer/SpryStore.PresentationLayer/Startup.cs
@@ -7,7 +7,10 @@ using Microsoft.Extensions.Hosting;
 using SpryStore.BusinessLayer.Abstract;
 using SpryStore.BusinessLayer.Concrete;
 using SpryStore.DataAccessLayer.Abstract;
+using SpryStore.DataAccessLayer.Concrete;
 using SpryStore.DataAccessLayer.EntityFramework;
+using SpryStore.EntityLayer.Concrete;
+using SpryStore.PresentationLayer.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -39,6 +42,9 @@ namespace SpryStore.PresentationLayer
             services.AddScoped<IContactDal, EfContactDal>();
             services.AddScoped<IContactService, ContactManager>();
 
+            services.AddDbContext<Context>();
+            services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<Context>().AddErrorDescriber<CustomIdentityValidator>();
+
             services.AddControllersWithViews();
         }
 
@@ -55,11 +61,13 @@ namespace SpryStore.PresentationLayer
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseStatusCodePagesWithReExecute("/ErrorPage/Page404","?code={0}");
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
