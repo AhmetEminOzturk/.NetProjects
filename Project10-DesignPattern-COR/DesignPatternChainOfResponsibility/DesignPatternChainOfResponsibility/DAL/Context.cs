2464d7f20ddc0ead4adf8542886c2b95207d1eff
AhmetEminOzturk <116569921+AhmetEminOzturk@users.noreply.github.com>
2023-05-05 17:16:20 +0300

    upd

---
+++ Project10-DesignPattern-COR/DesignPatternChainOfResponsibility/DesignPatternChainOfResponsibility/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPatternChainOfResponsibility.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-OQD43KV\\SQLEXPRESS; initial catalog=ChainOfRespDb; integrated security=true");
+        }
+        public DbSet<CustomerProcess> CustomerProcesses { get; set; }
+    }
+}
