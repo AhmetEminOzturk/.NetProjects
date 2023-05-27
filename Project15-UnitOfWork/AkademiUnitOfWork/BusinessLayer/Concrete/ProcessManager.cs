25beb51dcf16f86bdc7db60d4c166756de163889
AhmetEminOzturk <116569921+AhmetEminOzturk@users.noreply.github.com>
2023-05-28 02:12:30 +0300

    upd

---
+++ Project15-UnitOfWork/AkademiUnitOfWork/BusinessLayer/Concrete/ProcessManager.cs
@@ -0,0 +1,58 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.UnitOfWork;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ProcessManager : IProcessService
+    {
+        private readonly IProcessDal _processDall;
+        private readonly IUowDal _uowDal;
+
+        public ProcessManager(IProcessDal processDall, IUowDal uowDal)
+        {
+            _processDall = processDall;
+            _uowDal = uowDal;
+        }
+
+        public void TDelete(Process t)
+        {
+            _processDall.Delete(t);
+            _uowDal.Save();
+        }
+
+        public Process TGetByID(int id)
+        {
+            return _processDall.GetByID(id);
+        }
+
+        public List<Process> TGetList()
+        {
+            return _processDall.GetList();
+        }
+
+        public void TInsert(Process t)
+        {
+            _processDall.Insert(t);
+            _uowDal.Save();
+        }
+
+        public void TMultiUpdate(List<Process> t)
+        {
+            _processDall.MultiUpdate(t);
+            _uowDal.Save();
+        }
+
+        public void TUpdate(Process t)
+        {
+            _processDall.Update(t);
+            _uowDal.Save();
+        }
+    }
+}
---
+++ Project15-UnitOfWork/AkademiUnitOfWork/PresantationLayer/Controllers/DefaultController.cs
@@ -0,0 +1,59 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using PresantationLayer.Models;
+
+namespace PresantationLayer.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly ICustomerService _customerService;
+
+        public DefaultController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _customerService.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CustomerAdd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CustomerAdd(Customer customer)
+        {
+            _customerService.TInsert(customer);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult SendMoney()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SendMoney(CustomerViewModel customerViewModel)
+        {
+            var value1 = _customerService.TGetByID(customerViewModel.SenderID);
+            var value2 = _customerService.TGetByID(customerViewModel.ReceiverID);
+
+            value1.CustomerBalance -= customerViewModel.Amount;
+            value2.CustomerBalance += customerViewModel.Amount;
+
+            List<Customer> modifiedCustomers = new List<Customer>()
+            {
+                value1,
+                value2
+            };
+            _customerService.TMultiUpdate(modifiedCustomers);
+            return RedirectToAction("Index");
+        }
+    }
+}
